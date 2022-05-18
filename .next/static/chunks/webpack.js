9e6f9a83a34616d7f7774474eca451ad9f5a301d
esubalew.seife@gmail.com <esubalew.seife@gmail.com>
2022-05-18 09:37:46 -0700

    linking between pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "36405e7827541cbd"; }
+/******/ 		__webpack_require__.h = function() { return "05f1f68bd0cb7391"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,19 @@
+import { Fragment } from "react";
+import Link from "next/link";
 const newsPage = () => {
-  return <h1>This is news page</h1>;
+  return (
+    <Fragment>
+      <h1>This is news page</h1>
+      <ul>
+        <li>
+          <Link href="news/nextjs">Next Js is amazing</Link>
+        </li>
+        <li>
+          <Link href="news/google">Google engineer</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default newsPage;
